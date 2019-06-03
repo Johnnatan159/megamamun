7c8c21e1dabd287bb265f966c62a054eba2198ba
mamunmohamed <x9767907z@iespoblenou.org>
2019-06-03 20:54:52 +0200

    Prefabs del enemigo

---
+++ Assets/FireEnemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireEnemy : MonoBehaviour
+{
+
+    public GameObject bullet;
+    public float interval = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Fire", interval, interval);
+    }
+
+    // Update is called once per frame
+    void Fire()
+    {
+        //Spawn the bullet
+        GameObject g = Instantiate(bullet, transform.position, Quaternion.identity);
+
+        //Ignore Bullet<--> Enemy collision
+
+        Physics2D.IgnoreCollision(g.GetComponent<Collider2D>(),
+        transform.parent.GetComponent<Collider2D>());
+    }
+}
