9cd24c74113a7bff93c6524c93b92744a2ce7b5c
mamunmohamed <x9767907z@iespoblenou.org>
2019-06-03 02:33:40 +0200

    Creamos moviemientos para el enemigo

---
+++ Assets/Mamun/enemy_movement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_movement : MonoBehaviour
+{
+
+
+    public LayerMask enemyMask;
+    Rigidbody2D myBody;
+    Transform myTrans;
+    float myWidth;
+    public float speed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        myTrans = this.transform;
+        myBody = this.GetComponent<Rigidbody2D>();
+        myWidth = this.GetComponent<SpriteRenderer>().bounds.extents.x;
+
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        Vector2 lineCastPos = myTrans.position - myTrans.right * myWidth;
+        //Debug.DrawLine(lineCastPos, lineCastPos + Vector2.down);
+
+        bool isGrounded = Physics2D.Linecast(lineCastPos, lineCastPos + Vector2.down, enemyMask);
+
+        if (!isGrounded)
+        {
+            Vector3 currRot = myTrans.eulerAngles;
+            currRot.y += 180;
+            myTrans.eulerAngles = currRot;
+        }
+
+        Vector2 myVel = myBody.velocity;
+        myVel.x =  -myTrans.right.x * speed;
+        myBody.velocity = myVel;
+    }
+}
